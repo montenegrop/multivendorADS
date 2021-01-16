c310d9114234f6df7aa03415061e643e9ebef88e
montenegrop <pablormontenegro1@gmail.com>
2021-01-16 03:41:06 -0300

    improved imoport, added Decimal to prices

--- saleor/csv/utils/import_products.py
+++ saleor/csv/utils/import_products.py
@@ -10,18 +10,23 @@ from saleor.settings import DEFAULT_CURRENCY
 from saleor.channel.models import Channel
 import datetime
 from django.core.exceptions import ObjectDoesNotExist
+from decimal import Decimal
 
 
 # variant attributes:
-VARIANT_SKU = {"header": "variant sku", "attribute": "sku_simple"}
+VARIANT_SKU = {"header": "SKU", "attribute": "sku_simple"}
+VARIANT_NAME = {"header": "variante", "attribute": "name"}
 
 # products attributes:
 PRODUCT_ID = {"header": "id", "attribute": "id_simple"}
-PRODUCT_NAME = {"header": "name", "attribute": "name"}
-PRODUCT_TYPE_NAME = {"header": "product type", "attribute": "product_type"}
+PRODUCT_NAME = {"header": "nombre", "attribute": "name"}
+PRODUCT_TYPE_NAME = {"header": "categoria", "attribute": "product_type"}
+PRODUCT_DESCRIPTION = {"header": "descripcion", "attribute": "description"}
+
+# channel:
+CHANNEL_CURRENCY = {"header": "moneda", "attribute": "currency"}
 
 # product channel listing:
-CHANNEL_CURRENCY = {"header": "moneda", "attribute": "currency"}
 CHANNEL_VISIBLE_IN_LISTINGSS = {
     "header": "publicar", "attribute": "visible_in_listings"}
 VISIBLE = True
@@ -32,25 +37,48 @@ PUBLISHED = True
 PRICE_AMOUNT = {"header": "precio", "attribute": "price_amount"}
 COST_PRICE_AMOUNT = {"header": "costo", "attribute": "cost_price_amount"}
 
-# validaciones:
-
 
 def row_to_object(headers, data, vendor_id):
 
-    sku_simple = None
     id_simple = None
-    currency = DEFAULT_CURRENCY
+    product_type_name = None
+    product_name = None
+    sku_simple = None
+    variant_name = None
+    description = None
+    # stocks:
+    # peso:
+    # imagen:
+    # publicar:
     price_amount = None
     cost_price_amount = None
+    currency = DEFAULT_CURRENCY
+    # moneda:
     for i, header in enumerate(headers):
-        sku_simple = data[i] if header == VARIANT_SKU['header'] else sku_simple
         id_simple = data[i] if header == PRODUCT_ID['header'] else id_simple
+        product_type_name = data[i] if header == PRODUCT_TYPE_NAME['header'] else product_type_name
+        product_name = data[i] if header == PRODUCT_NAME['header'] else product_name
+        sku_simple = data[i] if header == VARIANT_SKU['header'] else sku_simple
+        variant_name = data[i] if header == VARIANT_NAME['header'] else variant_name
+        description = data[i] if header == PRODUCT_DESCRIPTION['header'] else description
+        price_amount = Decimal(
+            data[i]) if header == PRICE_AMOUNT['header'] and data[i] else price_amount
+        cost_price_amount = Decimal(
+            data[i]) if header == COST_PRICE_AMOUNT['header'] and data[i] else cost_price_amount
         currency = data[i] if header == CHANNEL_CURRENCY['header'] else currency
-        price_amount = data[i] if header == PRICE_AMOUNT['header'] else price_amount
-        cost_price_amount = data[i] if header == COST_PRICE_AMOUNT['header'] else cost_price_amount
 
+    # Retrieve channel:
     channel = Channel.objects.get(currency_code=currency)
 
+    # Retrieve or define variant:
+    try:
+        product_variant = ProductVariant.objects.get(
+            sku_simple=sku_simple, product__vendor_id=vendor_id)
+    except ObjectDoesNotExist:
+        sku = sku_simple + '00000' + str(vendor_id)
+        product_variant = ProductVariant(sku=sku, sku_simple=sku_simple)
+
+    # variant validations:
     if sku_simple == ' ':
         raise Exception('invalid SKU')
 
@@ -58,33 +86,27 @@ def row_to_object(headers, data, vendor_id):
         print('not SKU OR ID')
         return
 
-    # Retrieve or define variant:
-    if ProductVariant.objects.filter(
-            sku_simple=sku_simple, product__vendor_id=vendor_id).exists():
-        product_variant = ProductVariant.objects.get(
-            sku_simple=sku_simple, product__vendor_id=vendor_id)
-    else:
-        sku = sku_simple + '00000' + str(vendor_id)
-        product_variant = ProductVariant(sku=sku, sku_simple=sku_simple)
-
     # Retrieve or define product, aslo check if product id is valid:
-    if product_variant.product_id:
+    try:
         product = product_variant.product
-        if id_simple and product_variant.product.id_simple != id_simple:
-            raise Exception(
-                'el id del producto no corresponde con el producto del variant')
-    else:
+    except ObjectDoesNotExist:
         vendor = Vendor.objects.get(id=vendor_id)
         product = Product(id_simple=id_simple, vendor=vendor)
+    product_type = ProductType.objects.get(name=product_type_name)
+    product.product_type = product_type
+    product.name = product_name
+
+    # # product validation:
+    # if id_simple and product_variant.product.id_simple != id_simple:
+    #     raise Exception('el id del producto no corresponde con el producto del variant')
 
     # product_channel_listing:
     try:
-        product_channel = product.channel_listings.get(currency=currency)
+        product_channel = product.channel_listings.get(channel=channel)
     except ObjectDoesNotExist:
-        product_channel = ProductChannelListing(currency=currency)
-    product_channel.product = product
-    product_channel.__setattr__(CHANNEL_VISIBLE_IN_LISTINGSS['attribute'], VISIBLE)
-    product_channel.__setattr__(CHANNEL_IS_PUBLISHED['attribute'], PUBLISHED)
+        product_channel = ProductChannelListing(channel=channel, product=product)
+    product_channel.visible_in_listings = VISIBLE
+    product_channel.is_published = PUBLISHED
     product_channel.available_for_purchase = datetime.date.today()
     product_channel.channel = channel
 
@@ -93,34 +115,17 @@ def row_to_object(headers, data, vendor_id):
         product_variant_channel = product_variant.channel_listings.get(
             currency=currency)
     except ObjectDoesNotExist:
-        product_variant_channel = ProductVariantChannelListing(currency=currency)
-    product_variant_channel.variant = product_variant
-    product_variant_channel.__setattr__(PRICE_AMOUNT['attribute'], price_amount)
-    product_variant_channel.__setattr__(
-        COST_PRICE_AMOUNT['attribute'], cost_price_amount)
+        product_variant_channel = ProductVariantChannelListing(
+            currency=currency, variant=product_variant)
+    product_variant_channel.price_amount = price_amount
+    product_variant_channel.cost_price_amount = cost_price_amount
     product_variant_channel.channel = channel
 
-    for n, attr_name in enumerate(headers):
-        value = data[n]
-        try:
-            if attr_name == PRODUCT_TYPE_NAME['header']:
-                product_type = ProductType.objects.get(
-                    name=value)
-                product.product_type = product_type
-                continue
-
-            if attr_name == PRODUCT_NAME['header']:
-                product.__setattr__(PRODUCT_NAME['attribute'], value)
-                continue
-
-        except KeyError:
-            continue
-        else:
-            print("no exceptions")
     product.save()
-    product_channel.save()
     product_variant.product = product
     product_variant.save()
+    product_channel.save()
+    product_variant_channel.save()
 
     return
 
