4e9dacaf74dc38fa9a11b0a32426f228cc8051a4
montenegrop <pablormontenegro1@gmail.com>
2021-03-01 10:39:15 -0300

    vendor schema fix

--- saleor/graphql/vendor/mutations/vendors.py
+++ saleor/graphql/vendor/mutations/vendors.py
@@ -1,40 +1,42 @@
-import graphene
-from django.db import transaction
+# no se por que no funciona desde este archivo
+
+# import graphene
+# from django.db import transaction
 
 
-from saleor.vendors.models import Vendor
-from saleor.graphql.core.mutations import BaseMutation, ModelDeleteMutation, ModelMutation
+# from saleor.vendors.models import Vendor
+# from saleor.graphql.core.mutations import BaseMutation, ModelDeleteMutation, ModelMutation
 
 
-class VendorInput(graphene.InputObjectType):
-    description = graphene.String(description="Vendor description (HTML/text).")
-    name = graphene.String(description="Vendor name.")
-    slug = graphene.String(description="Vendor slug.")
+# class VendorInput(graphene.InputObjectType):
+#     description = graphene.String(description="Vendor description (HTML/text).")
+#     name = graphene.String(description="Vendor name.")
+#     slug = graphene.String(description="Vendor slug.")
 
 
-class VendorRegister(ModelMutation):
-    class Arguments:
-        input = VendorInput(
-            description="Fields required to create a vendor.", required=True
-        )
+# class VendorRegister(ModelMutation):
+#     class Arguments:
+#         input = VendorInput(
+#             description="Fields required to create a vendor.", required=True
+#         )
 
-    class Meta:
-        description = "Creates a new vendor."
-        # exclude = ["password"]
-        model = Vendor
-        # error_type_class = AccountError
-        # error_type_field = "account_errors"
+#     class Meta:
+#         description = "Creates a new vendor."
+#         # exclude = ["password"]
+#         model = Vendor
+#         # error_type_class = AccountError
+#         # error_type_field = "account_errors"
 
-    @classmethod
-    def mutate(cls, root, info, **data):
-        response = super().mutate(root, info, **data)
-        return response
+#     @classmethod
+#     def mutate(cls, root, info, **data):
+#         response = super().mutate(root, info, **data)
+#         return response
 
-    @classmethod
-    def clean_input(cls, info, instance, data):
-        return super().clean_input(info, instance, data)
+#     @classmethod
+#     def clean_input(cls, info, instance, data):
+#         return super().clean_input(info, instance, data)
 
-    @classmethod
-    @transaction.atomic
-    def save(cls, info, instance, cleaned_input):
-        instance.save()
+#     @classmethod
+#     @transaction.atomic
+#     def save(cls, info, instance, cleaned_input):
+#         instance.save()
--- saleor/graphql/vendor/schema.py
+++ saleor/graphql/vendor/schema.py
@@ -5,7 +5,6 @@ from graphene_django.types import DjangoObjectType, ObjectType
 
 from saleor.vendors.models import Vendor
 from saleor.graphql.core.mutations import BaseMutation, ModelDeleteMutation, ModelMutation
-from .mutations.vendors import VendorRegister
 
 
 class VendorType(DjangoObjectType):
@@ -16,9 +15,52 @@ class VendorType(DjangoObjectType):
 
 class VendorQueries(graphene.ObjectType):
     vendors = graphene.List(VendorType, description="list all vendors")
+    # vendor = graphene.Field(
+    #     Vendor,
+    #     id=graphene.Argument(
+    #         graphene.ID, description="ID of the vendor.", required=True),
+    #     description="Look up a vendor by ID.",
+    # )
 
     def resolve_vendors(self, info, **kwargs):
         return Vendor.objects.all()
+
+    # def resolve_vendor(self, info, id):
+    #     return Vendor.object.get(id=id)
+
+
+class VendorInput(graphene.InputObjectType):
+    description = graphene.String(description="Vendor description (HTML/text).")
+    name = graphene.String(description="Vendor name.")
+    slug = graphene.String(description="Vendor slug.")
+
+
+class VendorRegister(ModelMutation):
+    class Arguments:
+        input = VendorInput(
+            description="Fields required to create a vendor.", required=True
+        )
+
+    class Meta:
+        description = "Creates a new vendor."
+        # exclude = ["password"]
+        model = Vendor
+        # error_type_class = AccountError
+        # error_type_field = "account_errors"
+
+    @classmethod
+    def mutate(cls, root, info, **data):
+        response = super().mutate(root, info, **data)
+        return response
+
+    @classmethod
+    def clean_input(cls, info, instance, data):
+        return super().clean_input(info, instance, data)
+
+    @classmethod
+    @transaction.atomic
+    def save(cls, info, instance, cleaned_input):
+        instance.save()
 
 
 class VendorMutations(graphene.ObjectType):
