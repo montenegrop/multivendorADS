e0d62008eeb1a6f368825dd0624c10fa7b96b0ce
montenegrop <pablormontenegro1@gmail.com>
2021-04-11 16:39:36 -0300

    relevance field in vendor

--- saleor/graphql/vendor/schema.py
+++ saleor/graphql/vendor/schema.py
@@ -1,20 +1,61 @@
 import graphene
+from graphene_federation import key
+from graphene import relay
+from saleor.vendors import models
+
+from saleor.graphql.utils import get_user_or_app_from_context
+
+
+from saleor.graphql.meta.types import ObjectWithMetadata
+
 from django.db import transaction
+
+from saleor.graphql.channel.types import ChannelContextType, ChannelContextTypeWithMetadata
+
 
 from graphene_django.types import DjangoObjectType, ObjectType
 
-from saleor.vendors.models import Vendor
+from saleor.vendors.models import Vendor as VendorModel
 from saleor.graphql.core.mutations import BaseMutation, ModelDeleteMutation, ModelMutation
+
+from saleor.graphql.channel import ChannelContext, ChannelQsContext
+
+from saleor.graphql.core.fields import (
+    ChannelContextFilterConnectionField,
+    FilterInputConnectionField,
+    PrefetchingConnectionField,
+)
+
+from saleor.graphql.core.connection import CountableDjangoObjectType
 
 
 class VendorType(DjangoObjectType):
     class Meta:
-        description = "Vendors"
-        model = Vendor
+        description = "Vendorszz"
+        model = VendorModel
+
+
+@key(fields="id")
+class Vendor(ChannelContextTypeWithMetadata, CountableDjangoObjectType):
+
+    class Meta:
+        default_resolver = ChannelContextType.resolver_with_context
+        description = "Represents a vendor in the storefront."
+        interfaces = [relay.Node, ObjectWithMetadata]
+        model = VendorModel
+
+
+def resolve_vendors(
+    info, requestor, channel_slug=None, **_kwargs
+) -> ChannelQsContext:
+    qs = models.Vendor.objects.all()
+    return ChannelQsContext(qs=qs.distinct(), channel_slug=channel_slug)
 
 
 class VendorQueries(graphene.ObjectType):
-    vendors = graphene.List(VendorType, description="list all vendors")
+    vendors = ChannelContextFilterConnectionField(
+        Vendor, description="list all vendors")
+    # top_vendors = graphene.List(VendorType, description="list top ")
     # vendor = graphene.Field(
     #     Vendor,
     #     id=graphene.Argument(
@@ -22,11 +63,10 @@ class VendorQueries(graphene.ObjectType):
     #     description="Look up a vendor by ID.",
     # )
 
-    def resolve_vendors(self, info, **kwargs):
-        return Vendor.objects.all()
-
-    # def resolve_vendor(self, info, id):
-    #     return Vendor.object.get(id=id)
+    def resolve_vendors(self, info, channel=None, **kwargs):
+        requestor = get_user_or_app_from_context(info.context)
+        qs = VendorModel.objects.all()
+        return resolve_vendors(info, requestor, channel_slug=channel, **kwargs)
 
 
 class VendorInput(graphene.InputObjectType):
@@ -44,7 +84,7 @@ class VendorRegister(ModelMutation):
     class Meta:
         description = "Creates a new vendor."
         # exclude = ["password"]
-        model = Vendor
+        model = VendorModel
         # error_type_class = AccountError
         # error_type_field = "account_errors"
 
--- saleor/vendors/models.py
+++ saleor/vendors/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from saleor.core.models import ModelWithMetadata
 
 # Create your models here.
 
-class Vendor(models.Model):
+
+class Vendor(ModelWithMetadata):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=55, unique=True, allow_unicode=True)
     description = models.TextField(blank=True)
+    relevance = models.IntegerField(default=0)
