35054ca1f36c28bfcf8c6543bda69a5552e15cee
montenegrop <pablormontenegro1@gmail.com>
2021-04-11 17:19:23 -0300

    vendor main-image field

--- saleor/vendors/models.py
+++ saleor/vendors/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from saleor.core.models import ModelWithMetadata
+from versatileimagefield.fields import VersatileImageField
 
 # Create your models here.
 
@@ -9,3 +10,6 @@ class Vendor(ModelWithMetadata):
     slug = models.SlugField(max_length=55, unique=True, allow_unicode=True)
     description = models.TextField(blank=True)
     relevance = models.IntegerField(default=0)
+    main_image = VersatileImageField(
+        upload_to="vendor-main", blank=True, null=True
+    )
