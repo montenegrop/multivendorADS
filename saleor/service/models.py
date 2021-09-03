edfaf4e39466fb20f36ea1cec639d2c90d969f43
montenegrop <pablormontenegro1@gmail.com>
2021-09-03 18:35:48 -0300

    vendorReview Model

--- saleor/service/models.py
+++ saleor/service/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from saleor.account.models import User
 from saleor.vendors.models import Vendor
 from saleor.product.models import BaseProduct
+
+from versatileimagefield.fields import PPOIField, VersatileImageField
 # Create your models here.
 
 
@@ -19,3 +21,50 @@ class ServiceContract(models.Model):
     accepted_but_not_finished = models.BooleanField(default=False)
     user_reviewed = models.BooleanField(default=False)
     vendor_reviewed = models.BooleanField(default=False)
+
+
+CLIENT_UNAVAILABLE = 1
+VENDOR_UNAVAILABLE = 2
+JOB_NOT_POSSIBLE = 3
+OTHER = 4
+
+NO_COMPLETED = "No completa"
+
+
+class VendorContractReview(models.Model):
+    contract = models.ForeignKey(ServiceContract, on_delete=models.CASCADE)
+    concreted = models.BooleanField(default=False)
+
+    MOTIVES_CHOICES = [
+        (CLIENT_UNAVAILABLE, "Cliente no disponible"),
+        (VENDOR_UNAVAILABLE, "Prestador no se presenta"),
+        (JOB_NOT_POSSIBLE, "Trabajo no realizable"),
+        (OTHER, "Otro")
+    ]
+
+    motive = models.CharField(
+        max_length=40, choices=MOTIVES_CHOICES, default=OTHER)
+
+    other_motive = models.CharField(max_length=300, default=NO_COMPLETED)
+    title = models.CharField(max_length=300, default=NO_COMPLETED)
+    long_review = models.CharField(max_length=1000, default=NO_COMPLETED)
+
+
+class VendorContractReviewGeneralImage(models.Model):
+    image = VersatileImageField(
+        upload_to="services",
+        ppoi_field="ppoi",
+        blank=True,
+    )
+    ppoi = PPOIField()
+    alt = models.CharField(max_length=128, blank=True,
+                           default="foto de review del prestador de servicio")
+
+
+class VendorContractReviewImage(VendorContractReviewGeneralImage):
+    vendor_contract_review = models.ForeignKey(
+        VendorContractReview,
+        related_name="vendor_contract_review_images",
+        on_delete=models.CASCADE,
+    )
+    position = models.IntegerField()
